535275b20a50a3bf192b5a4e03e7ce0df119091c
sowngl123 <sowngl123@gmail.com>
2020-03-17 15:26:30 +0700

    3 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +11,19 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  product : Product = {
+    name: 'i team 1',
+    price: 24.24,
+    desc: 'nội dung sp',
+    img: 'http://placehold.it/700x400',
+    status: false
+  }
+  changeStatus(){
+    this.product.status = !this.product.status;
+  }
+  changeTitle(e){
+    this.product.name = e.target.value;
   }
 
 }
